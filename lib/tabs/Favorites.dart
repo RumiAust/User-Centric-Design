d8cfd236f772ed981da2b0afc05ad7c703aef713
RumiAust <rumi019aust@gmail.com>
2022-01-03 22:32:49 +0000

    drawer added

--- lib/tabs/Favorites.dart
+++ lib/tabs/Favorites.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food_roots/drawer/MyDrawerHeader.dart';
 
 class Favourites extends StatefulWidget {
   @override
@@ -9,12 +10,27 @@ class _FavouritesState extends State<Favourites> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Favorites'),automaticallyImplyLeading: false,centerTitle: true,),
+      appBar: AppBar(title: const Text('Favorites'),centerTitle: true,),
 
 
       body: const Center(
         child: Text('Favorite Screen', style: TextStyle(fontSize: 40),),
       ),
+      drawer: Drawer(
+        child: SingleChildScrollView(
+          child: Container(
+            child: Column(
+              children: [
+                MyDrawerHeader(),
+                MyDrawerList(),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
+  }
+  Widget MyDrawerList() {
+    return Container();
   }
 }
