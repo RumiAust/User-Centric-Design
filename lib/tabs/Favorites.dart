2ea18d2fe57b83eca2e871bce2dcf449091fbd65
MD Golam Muktadir <mdgolam.muktadir@siemens-healthineers.com>
2022-01-11 12:29:27 +0100

    app bar refactored

--- lib/tabs/Favorites.dart
+++ lib/tabs/Favorites.dart
@@ -10,24 +10,46 @@ class _FavouritesState extends State<Favourites> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Favorites'),centerTitle: true,),
+      appBar: AppBar(
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.settings,
+              color: Colors.white,
+            ),
+            onPressed: () {
+              // do something
+            },
+          )
+        ],
+        leading: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: CircleAvatar(
+            radius: 100,
+              backgroundImage:AssetImage('assets/Jennifer_Connelly.jpg'),
+        ),
+        ),
+        title: const Text('Favorites'),
+        centerTitle: true,
+      ),
+
 
 
       body: const Center(
         child: Text('Favorite Screen', style: TextStyle(fontSize: 40),),
       ),
-      drawer: Drawer(
-        child: SingleChildScrollView(
-          child: Container(
-            child: Column(
-              children: [
-                MyDrawerHeader(),
-                MyDrawerList(),
-              ],
-            ),
-          ),
-        ),
-      ),
+      // drawer: Drawer(
+      //   child: SingleChildScrollView(
+      //     child: Container(
+      //       child: Column(
+      //         children: [
+      //           MyDrawerHeader(),
+      //           MyDrawerList(),
+      //         ],
+      //       ),
+      //     ),
+      //   ),
+      // ),
     );
   }
   Widget MyDrawerList() {
